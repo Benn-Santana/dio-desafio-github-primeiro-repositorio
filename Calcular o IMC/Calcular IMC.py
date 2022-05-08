edd0bd61e69e19ea3e996765d5b55ff4cf00a165
Benn <benn122012@gmail.com>
2022-05-08 17:13:26 -0300

    atualização

--- Calcular o IMC/Calcular IMC.py
+++ Calcular o IMC/Calcular IMC.py
@@ -22,12 +22,12 @@ if imc <18.5:
       print(f'CONCLUSÃO: {nome} está abaixo do peso')
 elif imc>18.6 and imc <24.9 :
       print(f'CONCLUSÃO: {nome} está com o peso ideal')
-elif imc>25 and imc <29.9 :
+elif imc>24.9 and imc <29.9 :
       print(f'CONCLUSÃO: {nome} está levemente acima do peso')
-elif imc>30 and imc <34.9 :
+elif imc>29.9 and imc <34.9 :
       print(f'CONCLUSÃO: {nome} está obesidade grau 1')
-elif imc>35 and imc <39.9 :
-      print(f'CONCLUSÃO: {nome} está obesidade grau 2 - SEVERA')
+elif imc>34.9 and imc <39.9 :
+      print(f'CONCLUSÃO: {nome} está com obesidade grau 2 - SEVERA')
 else:
-      print(f'CONCLUSÃO: {nome} está obesidade grau 3 - MÓRBIDA')
+      print(f'CONCLUSÃO: {nome} está com obesidade grau 3 - MÓRBIDA')
 
